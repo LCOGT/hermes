4df32a3e8d213d869d47c79b8f2b5f621d262299
Lindy Lindstrom <llindstrom@lco.global>
2022-03-31 00:55:50 +0000

    set the STATIC_ROOT so collectstatic know where to copy files to

--- hermes_base/settings.py
+++ hermes_base/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_ROOT tells collectstatic where to copy all the static files that it collects
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
