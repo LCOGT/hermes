faa0b56a22212395e60cef4f8b43fa3c511d2c9b
Joseph Chatelain <jchatelain@lco.global>
2022-04-01 14:20:21 -0700

    add boilerplate form page

--- hermes/views.py
+++ hermes/views.py
@@ -1,7 +1,12 @@
-from hermes.models import Message
 from rest_framework import viewsets
+from django.views.generic import ListView, DetailView, FormView
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+import json
+
+from hermes.models import Message
+from hermes.forms import MessageForm
 from hermes.serializers import MessageSerializer
-from django.views.generic import ListView, DetailView
 
 
 class MessageViewSet(viewsets.ModelViewSet):
@@ -15,3 +20,27 @@ class MessageListView(ListView):
 
 class MessageDetailView(DetailView):
     model = Message
+
+
+class MessageFormView(FormView):
+    template_name = "hermes/generic_message.html"
+    form_class = MessageForm
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        super().form_valid(form)
+        new_message_data = form.cleaned_data
+
+        # List of universal Fields
+        non_json_fields = ['title', 'author', 'message_text']
+        non_json_dict = {key: new_message_data[key] for key in new_message_data.keys() if key in non_json_fields}
+        message = Message(**non_json_dict)
+
+        # convert form specific data to JSON
+        json_dict = {key: new_message_data[key] for key in new_message_data.keys() if key not in non_json_fields}
+        json_data = json.dumps(json_dict, indent=4)
+        message.data = json_data
+
+        message.save()
+
+        return redirect(self.get_success_url())
