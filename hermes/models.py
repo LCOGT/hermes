51ec102f027d4c5a13567274e2f9185cb3d6f916
Joseph Chatelain <jchatelain@lco.global>
2022-04-07 09:18:53 -0700

    add timestamp

--- hermes/models.py
+++ hermes/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from rest_framework.reverse import reverse
+import datetime
 
 
 class Message(models.Model):
@@ -7,6 +7,7 @@ class Message(models.Model):
     author = models.CharField(max_length=50, blank=True)
     data = models.JSONField(null=True)
     message_text = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
