84459c12f0b8c86688d03f5b427484f8f6857d57
Lindy Lindstrom <llindstrom@lco.global>
2022-04-13 19:16:41 +0000

    update Message model with new fields; follow convention for created

--- hermes/models.py
+++ hermes/models.py
@@ -1,12 +1,16 @@
+from pydoc_data.topics import topics
 from django.db import models
 
 
 class Message(models.Model):
-    title = models.CharField(max_length=50, blank=True)
-    author = models.CharField(max_length=50, blank=True)
+    topic = models.CharField(max_length=256, blank=True)
+    title = models.CharField(max_length=256, blank=True)
+    author = models.CharField(max_length=256, blank=True)
     data = models.JSONField(null=True)
     message_text = models.TextField(blank=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
+
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Time Created')
+    modified = models.DateTimeField(auto_now=True, verbose_name='Last Modified')
 
     def __str__(self):
-        return self.title
+        return f'{self.topic}: {self.title} from {self.author}'
