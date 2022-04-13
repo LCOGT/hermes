9e6503222ba48ac2c2c67911a204fe8e1c5c906c
Lindy Lindstrom <llindstrom@lco.global>
2022-04-13 16:58:56 +0000

    factor authentication into private method

--- hermes/management/commands/hopingest.py
+++ hermes/management/commands/hopingest.py
@@ -5,7 +5,11 @@ import logging
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 
-from  hop import Stream
+from hop import Stream
+from hop.auth import Auth
+from hop.io import StartPosition, Metadata
+from hop.models import GCNCircular
+
 
 logger = logging.getLogger(__name__)
 
@@ -22,7 +26,29 @@ class Command(BaseCommand):
         parser.add_argument('-e', '--earliest', required=False, default=False, action='store_true',
                             help='Read from the start of the Kafka stream with hop.io.StartPosition.EARLIEST')
 
-        #parser.add_argument('username', required=False, help='Username from scimma-admin')
+    def _get_hop_authentication(self, options):
+        """Get username and password and configure Hop client authentication
+
+        If command line arguments are supplied, use them.
+        Otherwise, get HOP_USERNAME and HOP_PASSWORD from environment.
+
+        For the moment, since there is not yet user account support,
+        HOP_USERNAME and HOP_PASSWORD should be set to enter the environment
+        up as k8s secrets.
+        """
+        username = options.get('username')
+        if username is None:
+            username = os.getenv('HOP_USERNAME', None)
+        password = options.get('password')
+        if password is None:
+            password = os.getenv('HOP_PASSWORD', None)
+
+        if username is None or password is None:
+            error_message = 'Supply Hop credentials on command line or set HOP_USERNAME and HOP_PASSWORD environment variables.'
+            logger.error(error_message)
+            raise CommandError(error_message)
+
+        return Auth(username, password)
 
 
     def handle(self, *args, **options):
