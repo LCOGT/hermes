e71835a67605c68f29fe85ad0da58234dc613837
Lindy Lindstrom <llindstrom@lco.global>
2022-04-20 16:46:07 +0000

    increase Message field lengths to accomodate gcn.circulars (emails)

--- hermes/models.py
+++ hermes/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class Message(models.Model):
     topic = models.CharField(max_length=256, blank=True)
     title = models.CharField(max_length=256, blank=True)
-    author = models.CharField(max_length=256, blank=True)
+    author = models.CharField(max_length=1024, blank=True)
     data = models.JSONField(null=True)
-    message_text = models.TextField(blank=True)
+    message_text = models.TextField(blank=True, max_length=2048)
 
     created = models.DateTimeField(auto_now_add=True, verbose_name='Time Created')
     modified = models.DateTimeField(auto_now=True, verbose_name='Last Modified')
