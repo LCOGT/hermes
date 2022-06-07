fef6dd11a670d51bc83598518f24917beffd7492
Lindy Lindstrom <llindstrom@lco.global>
2022-06-07 23:23:38 +0000

    install django-extenstion for shell_plus debugging
    this isn't strictly necessary and should be removed if
    it is not being used.

--- hermes_base/settings.py
+++ hermes_base/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'django_extensions',  # for debuging: shell_plus management command
     'bootstrap4',
     'rest_framework',
     'mozilla_django_oidc',
