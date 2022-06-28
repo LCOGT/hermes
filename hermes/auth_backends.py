c040a03f201cbc8db597e6e865d4ab2b3b302a03
Lindy Lindstrom <llindstrom@lco.global>
2022-06-28 00:51:50 +0000

    clean up logging and remove unneeded error_id

--- hermes/auth_backends.py
+++ hermes/auth_backends.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.core.exceptions import SuspiciousOperation, PermissionDenied
 
 import logging
-import secrets
 
 from mozilla_django_oidc import auth
 
@@ -22,10 +21,10 @@ class HopskotchOIDCAuthenticationBackend(auth.OIDCAuthenticationBackend):
     def __init__(self):
         auth.OIDCAuthenticationBackend.__init__(self)
         self.kafka_user_auth_group = settings.KAFKA_USER_AUTH_GROUP
-        logger.info(f'HopskotchOIDCAuthenticationBackend.__init__')
+        logger.debug(f'HopskotchOIDCAuthenticationBackend.__init__')
 
     def filter_users_by_claims(self, claims):
-        logger.info(f'HopskotchOIDCAuthenticationBackend.filter_users_by_claims: {claims}')
+        logger.debug(f'HopskotchOIDCAuthenticationBackend.filter_users_by_claims: {claims}')
         username = claims.get("vo_person_id")
         if not username:
             return self.UserModel.objects.none()
@@ -34,18 +33,17 @@ class HopskotchOIDCAuthenticationBackend(auth.OIDCAuthenticationBackend):
     def get_username(self, claims):
         """Return the vo_person_id in the claims
         """
-        logger.info(f'HopskotchOIDCAuthenticationBackend.get_username')
+        logger.debug(f'HopskotchOIDCAuthenticationBackend.get_username')
         return claims.get("vo_person_id")
 
     def verify_claims(self, claims):
         """
         """
-        logger.info(f'HopskotchOIDCAuthenticationBackend.verify_claims')
+        logger.debug(f'HopskotchOIDCAuthenticationBackend.verify_claims')
         # Value for 'is_member_of' key is  list(COManage groups)
         if "is_member_of" not in claims:
-            log_event_id = secrets.token_hex(8)
-            msg = f"Your account is missing LDAP claims. Are you sure you used the account you use for SCIMMA? Error ID: {log_event_id}"
-            logger.error(f"account is missing LDAP claims, error_id={log_event_id}, claims={claims}")
+            logger.error(f"Account is missing LDAP claims; claims={claims}")
+            msg = "Your account is missing LDAP claims. Are you sure you used the account you use for SCIMMA?"
             raise PermissionDenied(msg)
 
         for group in [self.kafka_user_auth_group]:
@@ -62,9 +60,8 @@ class HopskotchOIDCAuthenticationBackend(auth.OIDCAuthenticationBackend):
         if "email_list" in claims and len(claims.get("email_list", [])) > 0:
             return True
 
-        log_event_id = secrets.token_hex(8)
-        msg = f"Your account is missing an email claim. Error ID: {log_event_id}"
-        logger.error(f"account is missing LDAP email claims, error_id={log_event_id}, claims={claims}")
+        msg = f"Your account is missing an email claim; claims={claims}"
+        logger.error(f"Account is missing LDAP email claim; claims={claims}")
         raise PermissionDenied(msg)
 
     def create_user(self, claims):
@@ -73,7 +70,7 @@ class HopskotchOIDCAuthenticationBackend(auth.OIDCAuthenticationBackend):
              * email given by claims['email'] or claims['email_list][0]
              * is_staff is True for SCiMMA DevOps members
         """
-        logger.info(f'HopskotchOIDCAuthenticationBackend.create_user')
+        logger.debug(f'HopskotchOIDCAuthenticationBackend.create_user')
         if "email" in claims:
             email = claims.get("email")
         elif "email_list" in claims:
@@ -87,8 +84,8 @@ class HopskotchOIDCAuthenticationBackend(auth.OIDCAuthenticationBackend):
             email=email,
             is_staff=is_member_of(claims, 'CO:COU:SCiMMA DevOps:members:active'),
         )
-        logger.info(f'HopskotchOIDCAuthenticationBackend.create_user: new_user: {new_user}')
-        logger.info(f'HopskotchOIDCAuthenticationBackend.create_user: UserModel: {self.UserModel}')
+        logger.info(f'HopskotchOIDCAuthenticationBackend.create_user: new_user: {new_user} with claims: {claims}')
+        logger.debug(f'HopskotchOIDCAuthenticationBackend.create_user: UserModel: {self.UserModel}')
 
         return new_user
 
