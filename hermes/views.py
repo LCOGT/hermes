2de346af2e86c2892e2674ea01c4b874164148ea
Lindy Lindstrom <llindstrom@lco.global>
2022-10-18 23:54:55 +0000

    Allow SCiMMA Auth SCRAM credentials to come in via requeset header
    This is for API requests. See management/commands/hermestestsubmit.py
    for example code making this type of request.

--- hermes/views.py
+++ hermes/views.py
@@ -42,11 +42,27 @@ logger = logging.getLogger(__name__)
 
 
 def _extract_hop_auth(request) -> Auth:
-    """The hop.auth.Auth instance was inserted in to the Session dictionary upon logon
-    in AuthenticationBackend.authenticate. This method extracts it. jsons is used (not json)
-    because Auth is non-trivial to serialize/deserialize.
+    """Return a hop.Auth instance from either the request.header or the request.session.
+
+    The reqeust.header takes precidence over the request.session.
+
+    If this the request is comming from the HERMES front-end, then a hop.auth.Auth instance was inserted
+    into the request's session dictionary upon logon in AuthenticationBackend.authenticate.
+    This method extracts it. (`jsons` is used (vs. json) because Auth is non-trivial to
+    serialize/deserialize, and the stdlib `json` package won't handle it correctly).
+
+    If this this request is coming via the API, then a SCiMMA Auth SCRAM credential must be
+    extracted from the request header and then used to instanciate the returned hop.Auth.
     """
-    hop_user_auth: Auth = jsons.load(request.session['hop_user_auth_json'], Auth)
+    if 'SCIMMA-API-Auth-Username' in request.headers:
+        # A SCiMMA Auth SCRAM credential came in request.headers. Use it to get hop.auth.Auth instance.
+        username = request.headers['SCIMMA-API-Auth-Username']
+        password = request.headers['SCIMMA-API-Auth-Password']
+        hop_user_auth = Auth(username, password)
+    else:
+        # deserialize the hop.auth.Auth instance from the request.session
+        hop_user_auth: Auth = jsons.load(request.session['hop_user_auth_json'], Auth)
+
     return hop_user_auth
 
 
