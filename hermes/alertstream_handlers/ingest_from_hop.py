52d5f9f98f2b1480e8410a386d22e41ed5391815
Jon <jnation@lco.global>
2023-04-17 20:04:45 -0400

    Switch to using igwn.gwalert format and add required bookeeping for that

--- hermes/alertstream_handlers/ingest_from_hop.py
+++ hermes/alertstream_handlers/ingest_from_hop.py
@@ -4,22 +4,31 @@ from datetime import datetime, timezone
 from dateutil.parser import parse, parserinfo
 import logging
 import uuid
+import fastavro
+import healpy as hp
+import numpy as np
+import hashlib
+from astropy.table import Table
+from io import BytesIO
+from django.conf import settings
 
 from hop.io import Metadata
 from hop.models import GCNCircular, JSONBlob
 
-from hermes.models import Message
+from hermes.models import Message, NonLocalizedEvent, NonLocalizedEventSequence
 from hermes import parsers
 
 logger = logging.getLogger(__name__)
 
 GCN_CIRCULAR_PARSER = parsers.GCNCircularParser()
 HERMES_PARSER = parsers.HermesMessageParser()
+IGWN_ALERT_PARSER = parsers.IGWNAlertParser()
 
 TOPIC_PIECES_TO_IGNORE = [
     'gcn.notice',
     'heartbeat'
 ]
+
 
 def should_ingest_topic(topic):
     for topic_piece in TOPIC_PIECES_TO_IGNORE:
@@ -28,19 +37,103 @@ def should_ingest_topic(topic):
     return True
 
 
-def get_or_create_uuid_for_message(metadata: Metadata) -> uuid.UUID:
+def get_sequence_number(superevent_id: str) -> int:
+    """ This returns the sequence number of the next sequence for a superevent_id. This is a hack to get
+        around the fact that IGWN GWAlerts no longer tell you the sequence number within them.
+    """
+    try:
+        nle = NonLocalizedEvent.objects.get(event_id=superevent_id)
+        return nle.sequences.count() + 1
+    except NonLocalizedEvent.DoesNotExist:
+        return 1  # The nonlocalizedevent doesnt exist in our system yet, so this must be the first sequence
+
+
+def get_skymap_version(superevent_id: str, skymap_hash: uuid) -> int:
+    """ This method gets the most recent previous sequence of this superevent and checks if the skymap has changed.
+        It returns the 'version' of the skymap, which can be used to retrieve the proper file and image files from gracedb.
+    """
+    try:
+        nle = NonLocalizedEvent.objects.get(event_id=superevent_id)
+        latest_sequence = nle.sequences.last()
+        if latest_sequence.skymap_hash != skymap_hash:
+            return latest_sequence.skymap_version + 1
+        return latest_sequence.skymap_version
+    except NonLocalizedEvent.DoesNotExist:
+        return 0  # The nonlocalizedevent doesnt exist in our system yet, so this must be the first skymap version
+
+
+def get_combined_skymap_version(superevent_id: str, skymap_hash: uuid) -> int:
+    """ This method looks through the most recent previous sequences of this superevent and checks if the combined_skymap has changed.
+        It returns the 'version' of the skymap, which can be used to retrieve the proper file and image files from gracedb.
+    """
+    try:
+        nle = NonLocalizedEvent.objects.get(event_id=superevent_id)
+        for sequence in nle.sequences.all().reverse():
+            if sequence.combined_skymap_version and sequence.combined_skymap_hash and sequence.combined_skymap_hash != skymap_hash:
+                return sequence.combined_skymap_version + 1
+        return 0  # No previous combined_skymaps were found, so this must be the first sequence with a combined skymap
+    except NonLocalizedEvent.DoesNotExist:
+        return 0  # The nonlocalizedevent doesnt exist in our system yet, so this must be the first combined skymap version
+
+
+def get_confidence_regions(skymap: Table):
+    """ This helper method takes in the astropy Table skymap and attempts to parse out
+        the 50 and 90 area confidence values. It returns a tuple of (area_50, area_90).
+    """
+    try:
+        # Get the total number of healpixels in the map
+        n_pixels = len(skymap['PROBDENSITY'])
+        # Covert that to the nside parameter
+        nside = hp.npix2nside(n_pixels)
+        # Sort the probabilities so we can do the cumulative sum on them
+        probabilities = skymap['PROBDENSITY']
+        probabilities.sort()
+        # Reverse the list so that the largest pixels are first
+        probabilities = probabilities[::-1]
+        cumulative_probabilities = np.cumsum(probabilities)
+        # The number of pixels in the 90 (or 50) percent range is just given by the first set of pixels that add up
+        # to 0.9 (0.5)
+        index_90 = np.min(np.flatnonzero(cumulative_probabilities >= 0.9))
+        index_50 = np.min(np.flatnonzero(cumulative_probabilities >= 0.5))
+        # Because the healpixel projection has equal area pixels, the total area is just the heal pixel area * the
+        # number of heal pixels
+        healpixel_area = hp.nside2pixarea(nside, degrees=True)
+        area_50 = (index_50 + 1) * healpixel_area
+        area_90 = (index_90 + 1) * healpixel_area
+
+        return area_50, area_90
+    except Exception as e:
+        logger.error(f'Unable to parse raw skymap for OBJECT {skymap.meta["OBJECT"]} for confidence regions: {e}')
+
+    return None, None
+
+
+def get_sender_from_metadata(metadata: Metadata) -> str:
+    """ Extract a string sender from the message metadata
+    """
+    # get the tuple with the sender: key is '_sender'
+    sender_tuple = next((item for item in metadata.headers if item[0] == '_sender'), None)
+    if sender_tuple:
+        return sender_tuple[1].decode()
+    return ''
+
+
+def get_or_create_uuid_from_metadata(metadata: Metadata) -> uuid.UUID:
     """Extract the UUID from the message metadata, or generate a UUID if none present in metadata.
 
     The headers property of the metadata is a list of tuples of the form [('key', value), ...].
     """
     # get the tuple with the uuid: key is '_id'
-    message_uuid_tuple = next((item for item in metadata.headers if item[0] == '_id'), None)
+    message_uuid_tuple = None
+    if metadata.headers:
+        message_uuid_tuple = next((item for item in metadata.headers if item[0] == '_id'), None)
     if message_uuid_tuple:
         message_uuid = uuid.UUID(bytes=message_uuid_tuple[1])
     else:
         # this message header metadata didn't have UUID, so make one
         message_uuid = uuid.uuid4()
     return message_uuid
+
 
 def handle_generic_message(message: JSONBlob, metadata: Metadata):
     """Ingest a generic  alert from a topic we have no a priori knowledge of.
@@ -53,17 +146,16 @@ def handle_generic_message(message: JSONBlob, metadata: Metadata):
         message, created = Message.objects.update_or_create(
             # these fields must match for update...
             topic=topic,
-            uuid=get_or_create_uuid_for_message(metadata),
+            title='Generic Message',
+            uuid=get_or_create_uuid_from_metadata(metadata),
             published=published_time,
-            data=message.content,
-            defaults={
-                'message_text': str(metadata._raw.value())
-            }
+            data=message.content
         )
         if created:
             logger.debug(f'created new Message with id: {message.id} and uuid: {message.uuid}')
         else:
             logger.debug(f'found existing Message with and uuid: {message.uuid} id: {message.id}')
+
 
 def handle_gcn_circular_message(gcn_circular: GCNCircular, metadata: Metadata):
     """Add GNC Circular to Message db table (unless it already exists)
@@ -79,14 +171,13 @@ def handle_gcn_circular_message(gcn_circular: GCNCircular, metadata: Metadata):
 
     """
     logger.debug(f'updating db with gcn_circular number {gcn_circular.header["number"]}')
-
     # published date is in the header of the gcncircular
     published_time = parse(gcn_circular.header['date'], parserinfo=parserinfo(yearfirst=True))
 
     message, created = Message.objects.get_or_create(
         # fields to be compared to find existing Message (if any)
         topic=metadata.topic,
-        uuid=get_or_create_uuid_for_message(metadata),
+        uuid=get_or_create_uuid_from_metadata(metadata),
         message_text=gcn_circular.body,
         published=published_time,
         title=gcn_circular.header['subject'],
@@ -100,6 +191,72 @@ def handle_gcn_circular_message(gcn_circular: GCNCircular, metadata: Metadata):
         logger.debug(f'created new Message with id: {message.id} and uuid: {message.uuid}')
     else:
         logger.debug(f'found existing Message with and uuid: {message.uuid} id: {message.id}')
+
+
+def handle_igwn_message(message: JSONBlob, metadata: Metadata):
+    alert = message.content[0]
+    # Only store test alerts if we are configured to do so
+    if alert.get('superevent_id', '').startswith('M') and not settings.SAVE_TEST_MESSAGES:
+        return
+    # These alerts have a created timestamp, so use that for the published time instead of metadata timestamp
+    published_time: datetime.date = parse(alert.get('time_created'))
+    # Generate a descriptive title for these: event id + alert type
+    title = f"{alert.get('superevent_id')} - {alert.get('alert_type')}"
+    alert_uuid = get_or_create_uuid_from_metadata(metadata)
+
+    # Here we do a bit of pre-processing for IGWN alerts in order to be able to remove the skymap before saving the file
+    if alert.get('event'):
+        skymap_bytes = alert['event'].pop('skymap')
+        if skymap_bytes:
+            skymap = Table.read(BytesIO(skymap_bytes))
+            area_50, area_90 = get_confidence_regions(skymap)
+            if area_50:
+                alert['event']['area_50'] = area_50
+                alert['event']['area_90'] = area_90
+            skymap_hash = hashlib.md5(skymap_bytes)
+            skymap_version = get_skymap_version(alert['superevent_id'], skymap_hash=uuid.UUID(skymap_hash.hexdigest()))
+            alert['event']['skymap_hash'] = skymap_hash.hexdigest()
+            alert['event']['skymap_version'] = skymap_version
+            alert['urls']['skymap'] = f"https://gracedb.ligo.org/api/superevents/{alert['superevent_id']}/files/bayestar.multiorder.fits,{skymap_version}"
+    if alert.get('external_coinc', {}):
+        combined_skymap_bytes = alert['external_coinc'].pop('combined_skymap')
+        if combined_skymap_bytes:
+            combined_skymap = Table.read(BytesIO(combined_skymap_bytes))
+            area_50, area_90 = get_confidence_regions(combined_skymap)
+            if area_50:
+                alert['external_coinc']['area_50'] = area_50
+                alert['external_coinc']['area_90'] = area_90
+            combined_skymap_hash = hashlib.md5(combined_skymap_bytes)
+            combined_skymap_version = get_skymap_version(alert['superevent_id'], skymap_hash=uuid.UUID(combined_skymap_hash.hexdigest()))
+            alert['external_coinc']['combined_skymap_hash'] = combined_skymap_hash.hexdigest()
+            alert['external_coinc']['combined_skymap_version'] = combined_skymap_version
+            alert['urls']['combined_skymap'] = f"https://gracedb.ligo.org/api/superevents/{alert['superevent_id']}/files/combined-ext.multiorder.fits,{skymap_version}"
+    alert['sequence_num'] = get_sequence_number(alert['superevent_id'])
+
+    logger.debug(f"Storing message for igwn alert {alert_uuid}: {alert}")
+    try:
+        message, created = Message.objects.update_or_create(
+            # all these fields must match for update...
+            topic=metadata.topic,
+            uuid=alert_uuid,
+            title=title,
+            submitter=get_sender_from_metadata(metadata),
+            authors='LVK',
+            data=alert,
+            message_text='',
+            published=published_time,
+        )
+    except KeyError as err:
+        logger.error(f'Required key not found in {metadata.topic} alert: {alert_uuid}.')
+        return
+
+    IGWN_ALERT_PARSER.parse(message)
+
+    if created:
+        logger.debug(f'created new Message with id: {message.id} and uuid: {message.uuid}')
+    else:
+        logger.debug(f'found existing Message with and uuid: {message.uuid} id: {message.id}')
+
 
 def handle_hermes_message(hermes_message: JSONBlob,  metadata: Metadata):
     """Ingest a Hermes-published alert.
@@ -117,7 +274,7 @@ def handle_hermes_message(hermes_message: JSONBlob,  metadata: Metadata):
         message, created = Message.objects.update_or_create(
             # all these fields must match for update...
             topic=hermes_message.content['topic'],
-            uuid=get_or_create_uuid_for_message(metadata),
+            uuid=get_or_create_uuid_from_metadata(metadata),
             title=hermes_message.content['title'],
             submitter=hermes_message.content['submitter'],
             authors=hermes_message.content['authors'],
--- hermes/management/commands/inject_message.py
+++ hermes/management/commands/inject_message.py
@@ -1,14 +1,38 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.utils import timezone
 from dateutil.parser import parse
 from copy import deepcopy
 from hermes.models import Message
-from hermes.parsers import GCNLVCNoticeParser, GCNLVCCounterpartNoticeParser, GCNCircularParser
+from hermes.parsers import GCNLVCCounterpartNoticeParser, GCNCircularParser, IGWNAlertParser
 
+import uuid
 import logging
 logger = logging.getLogger(__name__)
 
-BASE_LVC_MESSAGE = 'TITLE:            GCN/LVC NOTICE\nNOTICE_DATE:      {published}\nNOTICE_TYPE:      Injected {type} Skymap\nTRIGGER_NUM:      {event_id}\nTRIGGER_DATE:     19896 TJD;   317 DOY;   2022/11/13 (yyyy/mm/dd)\nTRIGGER_TIME:     8982.000000 SOD (02:29:42.000000) UT\nSEQUENCE_NUM:     {sequence_number}\nGROUP_TYPE:       1 = CBC\nSEARCH_TYPE:      6 = MockDataChallenge\nPIPELINE_TYPE:    4 = gstlal\nFAR:              6.787e-14 [Hz]  (one per 170524293.0 days)  (one per 467189.84 years)\nPROB_NS:          1.00 [range is 0.0-1.0]\nPROB_REMNANT:     1.00 [range is 0.0-1.0]\nPROB_BNS:         0.99 [range is 0.0-1.0]\nPROB_NSBH:        0.00 [range is 0.0-1.0]\nPROB_BBH:         0.00 [range is 0.0-1.0]\nPROB_MassGap:     -1 [range is 0.0-1.0]   VALUE NOT ASSIGNED!\nPROB_TERRES:      0.00 [range is 0.0-1.0]\nTRIGGER_ID:       0x12\nMISC:             0x3C98E05\nSKYMAP_FITS_URL:  https://gracedb.ligo.org/api/superevents/MS221113c/files/bayestar.multiorder.fit\nEVENTPAGE_URL:    https://gracedb.ligo.org/superevents/MS221113c/view/\nCOMMENTS:         LVC Super Initial Skymap -- a location probability map.  \nCOMMENTS:            \nCOMMENTS:         NOTE: This is a TEST.  The role was test in the original voevent.  \nCOMMENTS:            \nCOMMENTS:         This event is an OpenAlert.  \nCOMMENTS:         LIGO-Hanford Observatory contributed to this candidate event.  \nCOMMENTS:         VIRGO Observatory contributed to this candidate event.  \nCOMMENTS:         This is an MS-series trigger (MS is for Mock Super Event).  \n'
+BASE_LVK_MESSAGE = {
+    'alert_type': 'PRELIMINARY',
+    'event': {'central_frequency': None,
+            'classification': {'BBH': 0.03,
+                                'BNS': 0.95,
+                                'NSBH': 0.01,
+                                'Terrestrial': 0.01},
+            'duration': None,
+            'far': 9.11069936486e-14,
+            'group': 'CBC',
+            'instruments': ['H1', 'L1', 'V1'],
+            'pipeline': 'gstlal',
+            'properties': {'HasMassGap': 0.01,
+                            'HasNS': 0.95,
+                            'HasRemnant': 0.91},
+            'search': 'MDC',
+            'significant': True,
+            'time': '2018-11-01T22:22:46.654Z'},
+    'external_coinc': None,
+    'superevent_id': 'MS181101ab',
+    'time_created': '2018-11-01T22:34:49Z',
+    'urls': {'gracedb': 'https://gracedb.ligo.org/superevents/MS181101ab/view/'}
+}
+
 BASE_LVC_COUNTERPART = 'TITLE:            GCN/LVC COUNTERPART NOTICE\nNOTICE_DATE:      {published}\nNOTICE_TYPE:      Injected {type}\nCNTRPART_RA:      {target_ra}d +19h 59m 32.4s (J2000),\n       300.0523d +20h 00m 12.5s (current),\n      299.4524d +19h 57m 48.5s (1950)\nCNTRPART_DEC:     {target_dec}d +40d 43 51.6 (J2000),\n   +40.7847d +40d 47 04.9 (current),\n    +40.5932d +40d 35 35.4 (1950)\nCNTRPART_ERROR:   7.6 [arcsec, radius]\nEVENT_TRIG_NUM:   {event_id}\nEVENT_DATE:       18599 TJD;   116 DOY;   2019/04/26 (yy/mm/dd)\nEVENT_TIME:       55315.00 SOD (15:21:55.00) UT\nOBS_DATE:         18599 TJD;   116 DOY;   19/04/26\nOBS_TIME:         73448.0 SOD (20:24:08.00) UT\nOBS_DUR:          72.7 [sec]\nINTENSITY:        1.00e-11 +/- 2.00e-12 [erg/cm2/sec]\nENERGY:           0.3-10 [keV]\nTELESCOPE:        Swift-XRT\nSOURCE_SERNUM:    {source_sernum}\nRANK:             2\nWARN_FLAG:        0\nSUBMITTER:        {author}\nSUN_POSTN:         34.11d (+02h 16m 26s)  +13.66d (+13d 39 45)\nSUN_DIST:          84.13 [deg]   Sun_angle= 6.3 [hr] (West of Sun)\nMOON_POSTN:       309.58d (+20h 38m 19s)  -19.92d (-19d 55 00)\nMOON_DIST:         61.34 [deg]\nMOON_ILLUM:       50 [%]\nGAL_COORDS:        76.19,  5.74 [deg] galactic lon,lat of the counterpart\nECL_COORDS:       317.73, 59.32 [deg] ecliptic lon,lat of the counterpart\nCOMMENTS:         LVC Counterpart.\nCOMMENTS:         This matches a catalogued X-ray source: 1RXH J195932.6+404351\nCOMMENTS:         This source has been given a rank of 2\nCOMMENTS:         Ranks indicate how likely the object is to be\nCOMMENTS:         the GW counterpart. Ranks go from 1-4 with\nCOMMENTS:         1 being the most likely and 4 the least.\nCOMMENTS:         See http://www.swift.ac.uk/ranks.php for details.\nCOMMENTS:         MAY match a known transient, will be checked manually.'
 BASE_GCN_CIRCULAR = {
     "header":
@@ -33,6 +57,8 @@ class Command(BaseCommand):
         parser.add_argument('-p', '--published', required=False, default=timezone.now().isoformat(), help='Published datetime for this Message')
         parser.add_argument('--sequence_number', required=False, default=1, help='Sequence number of injected message, only applies to LVC events.')
         parser.add_argument('--title', required=False, default='Injected Message', help='Title of injected message.')
+        parser.add_argument('--skymap_version', required=False, default=-1, help='Version of the skymap associated with this sequence of an LVK alert')
+        parser.add_argument('--combined_skymap_version', required=False, default=-1, help='Version of the combined skymap associated with this sequence of an LVK alert')
         parser.add_argument('--source_sernum', required=False, default=1, help='Sernum of target for LCV_COUNTERPART message.')
         parser.add_argument('--target_name', required=False, default='', help='Name of target to associate with message.')
         parser.add_argument('--target_ra', required=False, default=22.2, help='RA of target to associate with message.')
@@ -60,21 +86,32 @@ class Command(BaseCommand):
             )
             GCNLVCCounterpartNoticeParser().parse(message)
         elif 'LVC' in options.get('type'):
-            message_text = BASE_LVC_MESSAGE.format(
-                published=options.get('published'),
-                event_id=options.get('event_id'),
-                type=options.get('type'),
-                sequence_number=options.get('sequence_number')
-            )
+            base_type = options.get('type').split('_')[1]
+            message_payload = deepcopy(BASE_LVK_MESSAGE)
+            message_payload['alert_type'] = base_type
+            message_payload['superevent_id'] = options.get('event_id')
+            message_payload['time_created'] = options.get('published')
+            message_payload['sequence_num'] = options.get('sequence_number')
+            if options.get('skymap_version') >= 0:
+                message_payload['event']['skymap_version'] = options.get('skymap_version')
+                message_payload['event']['skymap_hash'] = uuid.uuid4().hex
+            if options.get('combined_skymap_version') >= 0:
+                message_payload['external_coinc'] = {
+                    'combined_skymap_version': options.get('skymap_version'),
+                    'combined_skymap_hash': uuid.uuid4().hex
+                }
+            title = f"{options.get('superevent_id')} - {base_type}"
             message, _ = Message.objects.get_or_create(
-                topic = options.get('type'),
-                message_text=message_text,
+                topic=options.get('type'),
+                title=title,
+                data=message_payload,
+                published=parse(options.get('published')),
                 defaults={
                     'submitter': 'inject_message command',
                     'authors': options.get('author')
                 }
             )
-            GCNLVCNoticeParser().parse(message)
+            IGWNAlertParser().parse(message)
         elif options.get('type') == 'GCN_CIRCULAR':
             header = deepcopy(BASE_GCN_CIRCULAR['header'])
             header['subject'] = header['subject'].format(event_id=options.get('event_id'))
