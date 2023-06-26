60ae972298c64ebc9e6e1575c33962c391647277
Jon <jnation@lco.global>
2023-06-26 19:33:11 -0400

    Fix multiline keys to have a newline between appended string values...

--- hermes/parsers/gcn_notice_plaintext_parser.py
+++ hermes/parsers/gcn_notice_plaintext_parser.py
@@ -88,8 +88,8 @@ class GCNNoticePlaintextParser(BaseParser):
                 if len(entry) > 1:
                     key = entry[0].strip().lower()
                     if key == last_entry and last_entry in parsed_fields:
-                        # For multi-line values repeating the key, append the values here
-                        parsed_fields[last_entry] += entry[1].lstrip()
+                        # For multi-line values repeating the key, append the values here with a space
+                        parsed_fields[last_entry] += f'\n{entry[1].lstrip()}'
                     else:
                         parsed_fields[key] = entry[1].strip()
                     last_entry = key
